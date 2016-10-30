f3699449773801a305df09264e4f5702dceac166
CDUTYangD <1442158384@qq.com>
2016-10-30 16:56:57 +0800

    修改窗体

--- pixChange/LayerMangerView.Designer.cs
+++ pixChange/LayerMangerView.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.ok = new System.Windows.Forms.Button();
             this.exCheckedListBox1 = new CustomControls.ExCheckedListBox();
             this.SuspendLayout();
@@ -43,23 +41,6 @@
             this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 1;
             this.label1.Text = "图层选择：";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 295);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 12);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "图层描述：";
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(15, 310);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(321, 98);
-            this.richTextBox1.TabIndex = 3;
-            this.richTextBox1.Text = "";
             // 
             // ok
             // 
@@ -78,7 +59,7 @@
             this.exCheckedListBox1.FormattingEnabled = true;
             this.exCheckedListBox1.Location = new System.Drawing.Point(15, 35);
             this.exCheckedListBox1.Name = "exCheckedListBox1";
-            this.exCheckedListBox1.Size = new System.Drawing.Size(322, 212);
+            this.exCheckedListBox1.Size = new System.Drawing.Size(322, 372);
             this.exCheckedListBox1.TabIndex = 6;
             this.exCheckedListBox1.Value = 0;
             // 
@@ -89,8 +70,6 @@
             this.ClientSize = new System.Drawing.Size(348, 420);
             this.Controls.Add(this.exCheckedListBox1);
             this.Controls.Add(this.ok);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "LayerMangerView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -104,8 +83,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         //private CustomControls.ExCheckedListBox exCheckedListBox1;
         private System.Windows.Forms.Button ok;
         private CustomControls.ExCheckedListBox exCheckedListBox1;
